8a1347b62ef7cf34cdec77aa8117f58ea4473558
thiagolider100 <thiagolider100@gmail.com>
2026-03-10 18:38:16 -0300

    Update Otimizar_2022.bat

--- Otimizar_2022.bat
+++ Otimizar_2022.bat
@@ -1,15 +1,28 @@
 @echo off
-:: Desativa animações para o RDP não travar
+title Otimizacao Extrema Windows 2022
+echo Aplicando ajustes de performance...
+
+:: 1. DESATIVAR ANIMACOES, TRANSPARENCIAS E SOMBRAS
 reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v "MinAnimate" /t REG_SZ /d 0 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ListviewAlphaSelect" /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t REG_DWORD /d 0 /f
+reg add "HKCU\Control Panel\Desktop" /v "UserPreferencesMask" /t REG_BINARY /d 9012038010000000 /f
 
-:: Remove o papel de parede (fundo preto = mais velocidade)
+:: 2. DESATIVAR SERVICOS (Update e Impressora)
+sc stop wuauserv
+sc config wuauserv start= disabled
+sc stop Spooler
+sc config Spooler start= disabled
+
+:: 3. REMOVER PAPEL DE PAREDE
 reg add "HKCU\Control Panel\Desktop" /v "Wallpaper" /t REG_SZ /d "" /f
 
-:: Prioridade total do processador para tarefas (Performance)
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d 38 /f
-
-:: Desativa o Defender para liberar RAM e CPU
+:: 4. DESATIVAR DEFENDER (Ganho de CPU)
 powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 
+:: 5. REINICIAR EXPLORER (Aplica as mudanças visuais na hora)
+taskkill /f /im explorer.exe
+start explorer.exe
+
+echo Otimizacao Concluida!
 exit
