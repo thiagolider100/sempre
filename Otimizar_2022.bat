c35c58bbb7a8a9c0c2faa8eb2b33523c064ec01d
thiagolider100 <thiagolider100@gmail.com>
2026-03-10 20:06:29 -0300

    Update Otimizar_2022.bat

--- Otimizar_2022.bat
+++ Otimizar_2022.bat
@@ -1,38 +1,34 @@
 @echo off
-title Otimizacao Extrema GitHub Actions
+title Otimizacao Extrema Windows 2022
 echo Aplicando ajustes de performance...
 
-:: 1. DESATIVAR WINDOWS UPDATE (Evita picos de CPU e disco)
+:: 1. DESATIVAR WINDOWS UPDATE (Evita picos de CPU)
 sc stop wuauserv
 sc config wuauserv start= disabled
 sc stop bits
 sc config bits start= disabled
-sc stop dosvc
-sc config dosvc start= disabled
 
-:: 2. DESATIVAR IMPRESSORA (Spooler)
+:: 2. DESATIVAR IMPRESSORA E BLUETOOTH
 sc stop Spooler
 sc config Spooler start= disabled
-
-:: 3. DESATIVAR BLUETOOTH (Inútil em VM)
 sc stop bthserv
 sc config bthserv start= disabled
 
-:: 4. DESATIVAR ANIMAÇÕES E TRANSPARÊNCIAS (RDP Fluido)
+:: 3. DESATIVAR ANIMACOES E TRANSPARENCIAS (RDP Fluido)
 reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v "MinAnimate" /t REG_SZ /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ListviewAlphaSelect" /t REG_DWORD /d 0 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ListviewAlphaSelect" /t REG_DWORD /d 0 /f
 
-:: 5. REMOVER PAPEL DE PAREDE (Fundo preto = Menos lag)
+:: 4. REMOVER PAPEL DE PAREDE (Fundo preto economiza banda)
 reg add "HKCU\Control Panel\Desktop" /v "Wallpaper" /t REG_SZ /d "" /f
 
-:: 6. PRIORIDADE DE PERFORMANCE E CPU
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d 38 /f
-
-:: 7. DESATIVAR DEFENDER (Ganho real de RAM)
+:: 5. DESATIVAR DEFENDER (Libera RAM e CPU)
 powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 
-:: 8. REINICIAR EXPLORER PARA APLICAR TUDO
+:: 6. PRIORIDADE DE PERFORMANCE
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d 38 /f
+
+:: 7. REINICIAR EXPLORER PARA APLICAR TUDO
 taskkill /f /im explorer.exe
 start explorer.exe
 
