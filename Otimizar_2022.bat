5636b60d5bb2b8e6d4f451523f38983537124bc9
thiagolider100 <thiagolider100@gmail.com>
2026-03-10 18:23:55 -0300

    Update Otimizar_2022.bat

--- Otimizar_2022.bat
+++ Otimizar_2022.bat
@@ -1,13 +1,15 @@
 @echo off
-reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
-reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
-reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v SecurityLayer /t REG_DWORD /d 1 /f
-netsh advfirewall firewall add rule name="RDP_Port" dir=in action=allow protocol=TCP localport=3389
-reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_SZ /d 506 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" /v VisualFXSetting /t REG_DWORD /d 2 /f
-reg add "HKCU\Control Panel\Desktop" /v UserPreferencesMask /t REG_BINARY /d 9012038010000000 /f
-reg add "HKCU\Control Panel\Desktop" /v FontSmoothing /t REG_SZ /d 0 /f
-reg add "HKCU\Software\Microsoft\Terminal Server Client\Default" /v DesktopWidth /t REG_DWORD /d 1200 /f
-reg add "HKCU\Software\Microsoft\Terminal Server Client\Default" /v DesktopHeight /t REG_DWORD /d 720 /f
-powercfg /hibernate off
+:: Desativa animações para o RDP não travar
+reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v "MinAnimate" /t REG_SZ /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ListviewAlphaSelect" /t REG_DWORD /d 0 /f
+
+:: Remove o papel de parede (fundo preto = mais velocidade)
+reg add "HKCU\Control Panel\Desktop" /v "Wallpaper" /t REG_SZ /d "" /f
+
+:: Prioridade total do processador para tarefas (Performance)
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d 38 /f
+
+:: Desativa o Defender para liberar RAM e CPU
+powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
+
 exit
