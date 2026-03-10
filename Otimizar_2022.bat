86e30b8ce4a553ba4e84ea0ef45b65b3552efa08
thiagolider100 <thiagolider100@gmail.com>
2026-03-10 18:59:53 -0300

    Update Otimizar_2022.bat

--- Otimizar_2022.bat
+++ Otimizar_2022.bat
@@ -1,18 +1,15 @@
 @echo off
-:: Desativa Animações e Transparências
+:: Desativa animações para o RDP não travar
 reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v "MinAnimate" /t REG_SZ /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ListviewAlphaSelect" /t REG_DWORD /d 0 /f
 
-:: Desativa Windows Update e Impressora
-sc stop wuauserv
-sc config wuauserv start= disabled
-sc stop Spooler
-sc config Spooler start= disabled
+:: Remove o papel de parede (fundo preto = mais velocidade)
+reg add "HKCU\Control Panel\Desktop" /v "Wallpaper" /t REG_SZ /d "" /f
 
-:: Desativa Defender
+:: Prioridade total do processador para tarefas (Performance)
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d 38 /f
+
+:: Desativa o Defender para liberar RAM e CPU
 powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 
-:: Reinicia Explorer para aplicar
-taskkill /f /im explorer.exe
-start explorer.exe
 exit
