0d08c8d2c7bc74e7d378947e731a671d9541ca61
thiagolider100 <thiagolider100@gmail.com>
2026-03-10 18:48:41 -0300

    Update Otimizar_2022.bat

--- Otimizar_2022.bat
+++ Otimizar_2022.bat
@@ -1,28 +1,18 @@
 @echo off
-title Otimizacao Extrema Windows 2022
-echo Aplicando ajustes de performance...
-
-:: 1. DESATIVAR ANIMACOES, TRANSPARENCIAS E SOMBRAS
+:: Desativa Animações e Transparências
 reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v "MinAnimate" /t REG_SZ /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ListviewAlphaSelect" /t REG_DWORD /d 0 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t REG_DWORD /d 0 /f
-reg add "HKCU\Control Panel\Desktop" /v "UserPreferencesMask" /t REG_BINARY /d 9012038010000000 /f
 
-:: 2. DESATIVAR SERVICOS (Update e Impressora)
+:: Desativa Windows Update e Impressora
 sc stop wuauserv
 sc config wuauserv start= disabled
 sc stop Spooler
 sc config Spooler start= disabled
 
-:: 3. REMOVER PAPEL DE PAREDE
-reg add "HKCU\Control Panel\Desktop" /v "Wallpaper" /t REG_SZ /d "" /f
-
-:: 4. DESATIVAR DEFENDER (Ganho de CPU)
+:: Desativa Defender
 powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 
-:: 5. REINICIAR EXPLORER (Aplica as mudanças visuais na hora)
+:: Reinicia Explorer para aplicar
 taskkill /f /im explorer.exe
 start explorer.exe
-
-echo Otimizacao Concluida!
 exit
