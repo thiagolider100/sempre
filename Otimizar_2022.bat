ab949ffe60ec88ea1cc7fb716f7bd237f1539ee8
thiagolider100 <thiagolider100@gmail.com>
2026-03-10 16:55:52 -0300

    Update Otimizar_2022.bat

--- Otimizar_2022.bat
+++ Otimizar_2022.bat
@@ -1,12 +1,13 @@
 @echo off
-net stop wuauserv /y >nul 2>&1
-sc config wuauserv start= disabled >nul 2>&1
-net stop bits /y >nul 2>&1
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f >nul 2>&1
-reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_SZ /d 506 /f >nul 2>&1
-reg add "HKCU\Software\Microsoft\Terminal Server Client\Default" /v DesktopWidth /t REG_DWORD /d 1200 /f >nul 2>&1
-reg add "HKCU\Software\Microsoft\Terminal Server Client\Default" /v DesktopHeight /t REG_DWORD /d 720 /f >nul 2>&1
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" /v VisualFXSetting /t REG_DWORD /d 2 /f >nul 2>&1
-reg add "HKCU\Control Panel\Desktop" /v FontSmoothing /t REG_SZ /d 0 /f >nul 2>&1
-powercfg /hibernate off >nul 2>&1
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v SecurityLayer /t REG_DWORD /d 1 /f
+netsh advfirewall firewall add rule name="RDP_Port" dir=in action=allow protocol=TCP localport=3389
+reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_SZ /d 506 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" /v VisualFXSetting /t REG_DWORD /d 2 /f
+reg add "HKCU\Control Panel\Desktop" /v UserPreferencesMask /t REG_BINARY /d 9012038010000000 /f
+reg add "HKCU\Control Panel\Desktop" /v FontSmoothing /t REG_SZ /d 0 /f
+reg add "HKCU\Software\Microsoft\Terminal Server Client\Default" /v DesktopWidth /t REG_DWORD /d 1200 /f
+reg add "HKCU\Software\Microsoft\Terminal Server Client\Default" /v DesktopHeight /t REG_DWORD /d 720 /f
+powercfg /hibernate off
 exit
